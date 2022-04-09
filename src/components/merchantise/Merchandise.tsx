e0c49f58f3a3537afca5e3f9a50ab0c8d1dbd7ea
Carl Wicker <carlwicker@gmail.com>
2022-04-09 18:19:41 +0100

    disabled merch data

--- src/components/merchantise/Merchandise.tsx
+++ src/components/merchantise/Merchandise.tsx
@@ -43,67 +43,65 @@ export default function Merchantise() {
       </Row>
 
       {itemArr.map((item: any, index: number) => {
-        return (
-          <div
-            key={index}
-            style={{
-              display: "flex",
-              alignItems: "start",
-              justifyContent: "start",
-            }}
-          >
-            <div
-              style={{
-                display: "flex",
-                flexDirection: "row",
-                //   backgroundColor: "grey",
-                justifyContent: "start",
-                alignItems: "center",
-                gap: "20px",
-                marginLeft: "150px",
-              }}
-            >
-              <div>
-                <img
-                  src={item?.image_link}
-                  style={{
-                    width: "150px",
-                    height: "150px",
-                    borderRadius: "20px",
-                    opacity: "0.75",
-                    backgroundColor: "red",
-                  }}
-                />
-              </div>
-
-              <div
-                style={{
-                  display: "flex",
-                  flexDirection: "column",
-                  justifyContent: "start",
-                  alignItems: "start",
-                }}
-              >
-                <div style={{ fontSize: "20px", fontWeight: "800" }}>
-                  {item?.title}
-                </div>
-                <div
-                  style={{
-                    fontSize: "20px",
-                    fontWeight: "100",
-                    marginBottom: "20px",
-                  }}
-                >
-                  {item?.price}
-                </div>
-
-                <a href={item?.link} target="_blank">
-                  <Button>Buy Now</Button>
-                </a>
-              </div>
-            </div>
-          </div>
-        );
+        // return (
+        //   <div
+        //     key={index}
+        //     style={{
+        //       display: "flex",
+        //       alignItems: "start",
+        //       justifyContent: "start",
+        //     }}
+        //   >
+        //     <div
+        //       style={{
+        //         display: "flex",
+        //         flexDirection: "row",
+        //         //   backgroundColor: "grey",
+        //         justifyContent: "start",
+        //         alignItems: "center",
+        //         gap: "20px",
+        //         marginLeft: "150px",
+        //       }}
+        //     >
+        //       <div>
+        //         <img
+        //           src={item?.image_link}
+        //           style={{
+        //             width: "150px",
+        //             height: "150px",
+        //             borderRadius: "20px",
+        //             opacity: "0.75",
+        //             backgroundColor: "red",
+        //           }}
+        //         />
+        //       </div>
+        //       <div
+        //         style={{
+        //           display: "flex",
+        //           flexDirection: "column",
+        //           justifyContent: "start",
+        //           alignItems: "start",
+        //         }}
+        //       >
+        //         <div style={{ fontSize: "20px", fontWeight: "800" }}>
+        //           {item?.title}
+        //         </div>
+        //         <div
+        //           style={{
+        //             fontSize: "20px",
+        //             fontWeight: "100",
+        //             marginBottom: "20px",
+        //           }}
+        //         >
+        //           {item?.price}
+        //         </div>
+        //         <a href={item?.link} target="_blank">
+        //           <Button>Buy Now</Button>
+        //         </a>
+        //       </div>
+        //     </div>
+        //   </div>
+        // );
       })}
     </Container>
   );
