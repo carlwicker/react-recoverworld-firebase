7ae7c05f075fde86ea902afe215373e0148e88cf
Carl Wicker <carlwicker@gmail.com>
2022-04-13 18:38:04 +0100

    wip home banner

--- src/components/radioBanner/RadioBanner.tsx
+++ src/components/radioBanner/RadioBanner.tsx
@@ -13,16 +13,27 @@ export default function RadioBanner() {
         style={{
           display: "flex",
           justifyContent: "flex-end",
+          alignItems: "center",
         }}
       >
         <img
           src="../../img/bannerTest1.png"
           style={{ height: "400px", opacity: 0.45 }}
         />
-        {/* <Carousel.Caption>
-          <h3>RECOVERWORLD RADIO</h3>
-          <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
-        </Carousel.Caption> */}
+        <Carousel.Caption
+          style={{
+            textAlign: "left",
+            alignItems: "center",
+            height: "400px",
+            display: "flex",
+          }}
+        >
+          <div style={{ display: "flex", flexDirection: "column", gap: "0" }}>
+            <h1 style={{ lineHeight: "0.8em" }}>RECOVERWORLD RADIO</h1>
+            <h3 style={{ lineHeight: "0.8em" }}>Every Friday @ 22:00GMT</h3>
+            <h4 style={{ lineHeight: "0.8em" }}>With Chris Hampshire</h4>
+          </div>
+        </Carousel.Caption>
       </Carousel.Item>
     </Carousel>
   );
