3d0014cb051a7d287e16fcb7bbda5be214958a55
Carl Wicker <carlwicker@gmail.com>
2022-04-15 00:24:59 +0100

    page length fix for paralax

--- src/components/paralaxBackground.tsx/ParalaxBackground.tsx
+++ src/components/paralaxBackground.tsx/ParalaxBackground.tsx
@@ -1,18 +1,19 @@
 import css from "./ParalaxBackground.module.css";
 
 export default function ParalaxBackground() {
-  let randomNum: number = Math.random() * (9000 - 2000) + 2000;
+  let randomNum: number = Math.random() * (9000 - 4500) + 4500;
 
   return (
     <img
       src="./img/massiveBack.png"
       style={{
+        display: "flex",
         width: "100%",
         position: "absolute",
         left: "0px",
         top: `-${randomNum}px`,
         zIndex: "-1",
-        opacity: "1",
+        opacity: "0.65",
         height: "fit-content",
         transformStyle: "preserve-3d",
         transform: "translateZ(-10px) scale(2)",
