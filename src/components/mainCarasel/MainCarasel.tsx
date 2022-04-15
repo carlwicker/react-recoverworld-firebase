ce1b27cd73a282f8a5866382cdebaca96ce90334
Carl Wicker <carlwicker@gmail.com>
2022-04-15 19:44:51 +0100

    carasel content alignment fix

--- src/components/mainCarasel/MainCarasel.tsx
+++ src/components/mainCarasel/MainCarasel.tsx
@@ -12,40 +12,42 @@ export default function RadioBanner() {
     >
       <Carousel.Item
         style={{
-          display: "flex",
-          justifyContent: "flex-end",
-          alignItems: "center",
+          // display: "flex",
+          // justifyContent: "center",
+          // alignItems: "center",
           height: "70vh",
         }}
       >
         {/* <img src="../../img/floatingCube.png" style={{ opacity: 0.95 }} /> */}
-        <Carousel.Caption
-          style={{
-            textAlign: "left",
-            alignItems: "center",
-            height: "fit-content",
-            display: "flex",
-          }}
-        >
-          <div
-            style={{
-              display: "flex",
-              flexDirection: "column",
-              gap: "0",
-              width: "100%",
-              opacity: ".95",
-              left: "0",
-            }}
-          >
-            <h1 style={{ lineHeight: "0.8em", overflowWrap: "break-word" }}>
-              <b>RECOVERWORLD</b> RADIO
-            </h1>
-            <p style={{ lineHeight: "1em" }}>Live every Friday @ 22:00GMT</p>
-            <div style={{ width: "fitContent" }}>
-              <Button>Listen Now</Button>
+        <Container>
+          <Carousel.Caption>
+            <div
+              style={{
+                display: "flex",
+                flexDirection: "column",
+                gap: "0",
+                width: "100%",
+                height: "70vh",
+                alignItems: "flex-start",
+                justifyContent: "center",
+                opacity: ".95",
+                top: "0",
+                left: "0",
+                textAlign: "left",
+                wordBreak: "break-word",
+                paddingTop: "150px",
+              }}
+            >
+              <h1 style={{ lineHeight: "0.8em", overflowWrap: "break-word" }}>
+                <b>RECOVERWORLD</b> RADIO
+              </h1>
+              <p>Live every Friday @ 22:00GMT</p>
+              <div style={{ width: "fitContent" }}>
+                <Button>Listen Now</Button>
+              </div>
             </div>
-          </div>
-        </Carousel.Caption>
+          </Carousel.Caption>
+        </Container>
       </Carousel.Item>
     </Carousel>
   );
