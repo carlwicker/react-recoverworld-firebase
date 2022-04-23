502c4d1286532cc83b3c8770f018c276fd504a6e
Carl Wicker <carlwicker@gmail.com>
2022-04-23 13:08:02 +0100

    get xml and convert to json cloud function completed

---
+++ functions/src/index.ts
@@ -0,0 +1,36 @@
+import * as functions from "firebase-functions";
+import axios from "axios";
+import { XMLParser } from "fast-xml-parser";
+
+const express = require("express");
+const app = express();
+let xmlDataToJson: any;
+
+const cors = require("cors")({ origin: true });
+app.use(cors);
+
+app.get("/importRelease/:id", (req: any, res: any, next: any) => {
+  const ampsuiteId: number = req.params.id;
+
+  function getXML() {
+    let jObj: any;
+    let url = `https://recoverworld.ampsuite.com/xml/releases?cid=10&id=${ampsuiteId}`;
+
+    axios
+      .get(url)
+      .then((response) => {
+        const xml: any = response.data;
+
+        const parser: any = new XMLParser();
+        if (xml) {
+          jObj = parser?.parse(xml);
+          xmlDataToJson = jObj.releases.release;
+        }
+        res.send(xmlDataToJson);
+      })
+      .catch((err) => next(err));
+  }
+  getXML();
+});
+
+exports.app = functions.https.onRequest(app);
