755a1575eb21cca79455b19c33c1dd45dbc9a7ab
Carl Wicker <carlwicker@gmail.com>
2022-04-25 19:03:41 +0100

    css hover color fix

--- src/components/social/Social.tsx
+++ src/components/social/Social.tsx
@@ -1,18 +1,23 @@
 import css from "./Social.module.css";
 import { BsFacebook, BsTwitter, BsInstagram, BsYoutube } from "react-icons/bs";
-import { Container } from "react-bootstrap";
 
 import { Link } from "react-router-dom";
 
 export default function Social() {
   return (
-    <Container>
-      <div style={{ display: "flex", gap: "10px" }} className="py-5">
+    <div style={{ display: "flex", gap: "10px" }}>
+      <Link to="http://www.facebook.com" target="_blank">
         <BsFacebook className={css["social-icon"]} />
+      </Link>
+      <Link to="http://www.twitter.com" target="_blank">
         <BsTwitter className={css["social-icon"]} />
+      </Link>
+      <Link to="http://www.instagram.com" target="_blank">
         <BsInstagram className={css["social-icon"]} />
+      </Link>
+      <Link to="http://www.youtube.com" target="_blank">
         <BsYoutube className={css["social-icon"]} />
-      </div>
-    </Container>
+      </Link>
+    </div>
   );
 }
