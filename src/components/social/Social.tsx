2cbdde169758491227525a626aa466f4d7ab5108
Carl Wicker <carlwicker@gmail.com>
2022-04-25 19:43:22 +0100

    label search filter fix

--- src/components/social/Social.tsx
+++ src/components/social/Social.tsx
@@ -1,23 +1,21 @@
 import css from "./Social.module.css";
 import { BsFacebook, BsTwitter, BsInstagram, BsYoutube } from "react-icons/bs";
 
-import { Link } from "react-router-dom";
-
 export default function Social() {
   return (
     <div style={{ display: "flex", gap: "10px" }}>
-      <Link to="http://www.facebook.com" target="_blank">
+      <a href="http://www.facebook.com" target="_blank">
         <BsFacebook className={css["social-icon"]} />
-      </Link>
-      <Link to="http://www.twitter.com" target="_blank">
+      </a>
+      <a href="http://www.twitter.com" target="_blank">
         <BsTwitter className={css["social-icon"]} />
-      </Link>
-      <Link to="http://www.instagram.com" target="_blank">
+      </a>
+      <a href="http://www.instagram.com" target="_blank">
         <BsInstagram className={css["social-icon"]} />
-      </Link>
-      <Link to="http://www.youtube.com" target="_blank">
+      </a>
+      <a href="http://www.youtube.com" target="_blank">
         <BsYoutube className={css["social-icon"]} />
-      </Link>
+      </a>
     </div>
   );
 }
